27f16fb2c0a4c29f1a7c255aa2adb2db9051a033
Eagle Engineering <eeuser@chaminet.net>
2019-09-10 17:00:00 -0700

    auton maker is updated

--- CreateRoutine.bat
+++ CreateRoutine.bat
@@ -1,1 +1,5 @@
-create routine program
+@echo off 
+set /p CODENAME= What is the code name (EX: 1138A-VEX-Code-2018-2019) :
+set /p AUTON= What is the name of your auton 
+echo //new auton .h > C:\Users\eeuser\Documents\GitHub\*CODENAME*\include\libIterativeRobot\commands\*AUTON*.h
+echo //new auton .h > C:\Users\eeuser\Documents\GitHub\*CODENAME*\src\libIterativeRobot\commands\*AUTON*.cpp

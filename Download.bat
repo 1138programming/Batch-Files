8ebb5c20c61128e0af3ec8b5c8d538a19da0513f
alexedw01 <32690377+alexedw01@users.noreply.github.com>
2019-09-20 07:31:29 -0700

    Fixing prosV5 upload line

--- Download.bat
+++ Download.bat
@@ -12,7 +12,7 @@ set /p DOWNLOAD= What is the file name of your code? (be specific) :
 cd %DOWNLOAD%
 set /p SLOT= What slot do you what the code to be on? 
 prosV5 make
-prosV5 upload --%SLOT% --%DOWNLOAD%
+prosV5 upload --slot %SLOT% --name "%DOWNLOAD%"
 choice /c YN /m "Did it work?"
 if %errorlevel%==1 exit
 
